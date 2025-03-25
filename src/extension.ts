68aff6ff5eff55e59e41faa89588e0c76c7b37a8
Andy Li <andy.li@wisetechglobal.com>
2025-03-25 16:33:51 +1030

    build and run now

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as child_process from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.runDotNetCode', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage('No active editor found. Open a .cs file to run.');
+            return;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dotnetquickrun" is now active!');
+        const document = editor.document;
+        const code = document.getText();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('dotnetquickrun.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dotnetQuickRun!');
-	});
+        const tempDir = path.join(context.extensionPath, 'temp');
+        if (!fs.existsSync(tempDir)) {
+            fs.mkdirSync(tempDir);
+        }
 
-	context.subscriptions.push(disposable);
+        const tempFile = path.join(tempDir, 'Program.cs');
+        fs.writeFileSync(tempFile, code);
+
+        const outputChannel = vscode.window.createOutputChannel('Run .NET Code');
+        outputChannel.show(true);
+        outputChannel.appendLine('Running .NET code...');
+
+        const command = `dotnet run --project ${tempDir}`;
+        child_process.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                outputChannel.appendLine(`Error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                outputChannel.appendLine(`stderr: ${stderr}`);
+                return;
+            }
+            outputChannel.appendLine(`Output: ${stdout}`);
+        });
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
